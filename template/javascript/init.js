2959d49e6ae48a520612f8baedd19bf8762ea5a5
Maeva RIDARD <maevaridard@iMac-de-Maeva.local>
2016-03-23 23:04:47 +0100

    Add a template

---
+++ template/javascript/init.js
@@ -0,0 +1,47 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  	});
+
+
+    $('select').material_select();
+
+    $('.suppr_product').click(function() {
+      $(this).parent().remove();
+    });
+
+        
+  }); // end of document ready
+
+
+})(jQuery); // end of jQuery name space
+
+var slider = document.getElementById('price_slider');
+  noUiSlider.create(slider, {
+   start: [20, 80],
+   connect: true,
+   margin: 1,
+   step: 1,
+   range: {
+     'min': 0,
+     'max': 100
+   }
+  });
+
+
+
+var valueMin = document.getElementById('value-min'),
+    valueMax = document.getElementById('value-max');
+
+slider.noUiSlider.on('update', function ( values, handle ) {
+	if ( handle ) {
+		valueMax.innerHTML = values[handle];
+	} else {
+		valueMin.innerHTML = values[handle];
+	}
+});
