84f1dfcc4ff7a113db57ee6fa896b3bfae0ff2ef
vqsto <romain.croizat@hotmail.fr>
2016-03-24 17:47:02 +0100

    localstorage get form + input handler

--- api/index.php
+++ api/index.php
@@ -9,6 +9,7 @@ ini_set('display_errors', 1);
 
 // APP
 require('handler/WebsiteHandler.php');
+require('handler/InputHandler.php');
 
 require('db.php');
 require('Toro.php');
@@ -22,6 +23,9 @@ ToroHook::add("404", function() {
 Toro::serve(array(
 
 	"1/website/:number"			=> "WebsiteHandler",			// GET
-	"1/website/"				=> "WebsiteHandler"				// POST
+	"1/website/"				=> "WebsiteHandler",			// POST
+
+	"1/input/:number"			=> "InputHandler",				// GET
+	"1/input/"					=> "InputHandler"				// POST
 
 ));
