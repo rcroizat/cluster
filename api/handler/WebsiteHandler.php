401a691dbb0e801e8a0a31bc0b8dcd92c70e586d
alaurelut <axel.laurelut@cifacom.com>
2016-03-25 19:24:30 +0100

    post data

--- api/handler/WebsiteHandler.php
+++ api/handler/WebsiteHandler.php
@@ -3,7 +3,7 @@
 class WebsiteHandler {
 	function post(){
 
-		$_POST = json_decode(file_get_contents('php://input'));
+		$_POST = (array) json_decode(file_get_contents('php://input'));
 		if(isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
 			$_POST = array_merge($_POST, (array) json_decode(trim(file_get_contents('php://input')), true));
 		}
