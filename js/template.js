9cdd8dc889e918bbc362c908a00b382a668db877
vqsto <romain.croizat@hotmail.fr>
2016-03-30 10:43:43 +0200

    push

--- js/template.js
+++ js/template.js
@@ -23,38 +23,43 @@ $(document).ready(function() {
 $(".send").on("click", function() {
 
     var  fields = JSON.parse(localStorage.getItem('field'));
-/*    var data = {
+
+    var data = {
         template: $('input[name=templateName]:checked').val(),
         category: localStorage.getItem('category'),
         description: localStorage.getItem('description'),
         name: localStorage.getItem('name'),
         type: localStorage.getItem('type'),
         url: localStorage.getItem('url')
-    };   */ 
-
-    var data = {
+    };    
+/*    var data = {
         template: 1,
         category: 2,
         description: 'DESCRIPTION TEST lLALALDFEG',
         name: localStorage.getItem('name'),
         type: 2,
         url: 'TESTURL'
-    };
+    };*//*
   $.each(fields, function(key, field) {
            console.log(field.val);
 
-        });
-/*
+        });*/
+
     $.post('http://vps261052.ovh.net/1/website/', function(data) {
         var id = data;
         $.each(fields, function(key, field) {
-            console.log('type '+ field.type+' val '+field.val)
-                $.post('1/input/', function({idWebSite : id, type : field.type, val : field.val}){
+            var type = field.type;
+            var val = field.val;
+                $.post('1/input/', 
+                    function({
+                        idWebSite : id, 
+                        type : type, 
+                        val : val}){
                     alert('good');
                     });
         });
 
-    });*/
+    });
 
 
 });
