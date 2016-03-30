b2d201a452848cb7c9a3b64a62011458735d9f19
alaurelut <axel.laurelut@cifacom.com>
2016-03-30 12:41:44 +0200

    return val theme handler

--- api/handler/ValThemeHandler.php
+++ api/handler/ValThemeHandler.php
@@ -13,29 +13,43 @@ class ValThemeHandler {
 		$valThemeName = trim($_POST['name']);
 		$valThemeValue = trim($_POST['value']);
 
-		$sql = "DELETE FROM `val_theme` WHERE `id_website` = :id_website;";
+	    $sql = "SELECT * FROM val_theme WHERE id_website = :id_website AND name = :name;";
+	    $query = $this->db->prepare($sql);
+	    $query->bindValue('id_website', $valThemeIdWebsite);
+	    $query->bindValue('name', $valThemeName);
+	    $query->execute();
 
-		$query_update = $this->db->prepare($sql);
-		$query_update->bindValue('id_website', $valThemeIdWebsite);
-		$query_update->execute();
-		
+	    if($query->rowCount() > 0){
 
-	 	$sql_insert = "INSERT INTO `val_theme` 
-	    ( `id_website`, `name`, `value`)
-	    VALUES (:id_website, :name, :value)";
+	    	$sql_update = "UPDATE `val_theme` SET `value` = :value WHERE `id_website` = :id_website AND `name` = :name;";
 
-	    $query_insert = $this->db->prepare($sql_insert);
-	    $query_insert->bindValue('id_website', (int) $valThemeIdWebsite, PDO::PARAM_INT);
-	    $query_insert->bindValue('name', $valThemeName, PDO::PARAM_STR);
-	    $query_insert->bindValue('value', $valThemeValue, PDO::PARAM_STR);
+		    $query_update = $this->db->prepare($sql_update);
+		    $query_update->bindValue('id_website', (int) $valThemeIdWebsite, PDO::PARAM_INT);
+		    $query_update->bindValue('name', $valThemeName, PDO::PARAM_STR);
+		    $query_update->bindValue('value', $valThemeValue, PDO::PARAM_STR);
 
-	    $query_insert->execute();
+		    $query_update->execute();
+	    } 
+	    else {
 
-	    $valThemeId = $this->db->lastInsertId();
+		 	$sql_insert = "INSERT INTO `val_theme` 
+		    ( `id_website`, `name`, `value`)
+		    VALUES (:id_website, :name, :value)";
+
+		    $query_insert = $this->db->prepare($sql_insert);
+		    $query_insert->bindValue('id_website', (int) $valThemeIdWebsite, PDO::PARAM_INT);
+		    $query_insert->bindValue('name', $valThemeName, PDO::PARAM_STR);
+		    $query_insert->bindValue('value', $valThemeValue, PDO::PARAM_STR);
+
+		    $query_insert->execute();
+
+		    $valThemeId = $this->db->lastInsertId();
+
+	   	}
 
 	    header("HTTP/1.1 200 OK");
 	    // header("HTTP/1.1 400 Bad Request");
-		echo json_encode(array('id' => $valThemeId, 'valThemeIdWebsite' => $valThemeIdWebsite, 'valThemeName' => $valThemeName, 'valThemeValue' => $valThemeValue, 'sql_insert' => $sql_insert ));
+		echo json_encode(array('success' => true);
 	}
 
 	function get($websiteId){
